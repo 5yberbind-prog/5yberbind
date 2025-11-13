f007b8630b8d5c3383c5404c44d4e04176c92541
Ashwani Kumar <5yberbind@gmail.com>
2025-11-14 03:06:09 +0530

    Update Style.js

--- Style.js
+++ Style.js
@@ -1,1 +1,19 @@
+async function loadData(){
+  const res = await fetch("data/items.json");
+  return await res.json();
+}
 
+function card(item){
+  return `
+    <div class="cat-box">
+      <img src="${item.thumbnail}">
+      <h3>${item.title}</h3>
+    </div>`;
+}
+
+async function render(){
+  let data = await loadData();
+  document.querySelector("#galleryGrid").innerHTML =
+    data.map(card).join("");
+}
+render();
