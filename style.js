0619fb961c1dce335a1fe3b257735aba8e2dbfc0
Ashwani Kumar <5yberbind@gmail.com>
2025-11-14 05:19:18 +0530

    Update style.js

--- style.js
+++ style.js
@@ -68,3 +68,22 @@
   });
 
 })();
+/* Neon click ripple + cursor boost */
+document.addEventListener("click", function(e) {
+    // Neon ripple
+    const ripple = document.createElement("div");
+    ripple.className = "click-ripple";
+    ripple.style.left = e.clientX + "px";
+    ripple.style.top = e.clientY + "px";
+    document.body.appendChild(ripple);
+
+    // remove ripple after animation
+    setTimeout(() => ripple.remove(), 600);
+
+    // Boost cursor glow effect
+    const cf = document.querySelector(".cursor-follower");
+    if (cf) {
+        cf.classList.add("click-boost");
+        setTimeout(() => cf.classList.remove("click-boost"), 200);
+    }
+});
