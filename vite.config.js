471f10b941c30f1dda3a54a9909f3084f33ad5cb
Ashwani Kumar <5yberbind@gmail.com>
2025-11-19 02:41:41 +0530

    Update vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Agar GitHub Pages pe deploy kar rahe ho, to `base` ko repo name pe set karo, e.g. "/5yberbind/"
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 }
-})
