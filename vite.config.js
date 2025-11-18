7214c3ecc0a9a4b6bc56f515e0fbcc3baa602601
Ashwani Kumar <5yberbind@gmail.com>
2025-11-18 15:25:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/5yberbind/",
+  base: "/5yberbind/",  // IMPORTANT
   plugins: [react()],
   server: { port: 5173 }
 })
