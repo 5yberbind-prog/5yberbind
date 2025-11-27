16cdc2ab4984244b81b928a8cf05dad8f1d530f4
Ashwani Kumar <5yberbind@gmail.com>
2025-11-27 15:22:29 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{jsx,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: { extend: {} },
+  plugins: []
 };
