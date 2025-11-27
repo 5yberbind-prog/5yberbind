f17d9a3639cd4a2b2ad15303a1f347fe70858bce
Ashwani Kumar <5yberbind@gmail.com>
2025-11-27 10:30:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,32 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail";
+import AppMarket from "./pages/AppMarket";
+import AppDetail from "./pages/AppDetail";
 
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      {/* Navbar */}
+      <nav className="bg-white shadow px-6 py-4 flex justify-between">
+        <Link to="/" className="font-bold text-xl">5yberBind</Link>
+        <div className="flex gap-4">
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/apps">Apps</Link>
+        </div>
+      </nav>
+
+      {/* Routes */}
+      <div className="max-w-6xl mx-auto mt-6 px-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<BlogList />} />
+          <Route path="/blogs/:id" element={<BlogDetail />} />
+          <Route path="/apps" element={<AppMarket />} />
+          <Route path="/apps/:id" element={<AppDetail />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
