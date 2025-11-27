0e6cfd2e6b27fe81b091c524a1a36ccdd68bb82b
Ashwani Kumar <5yberbind@gmail.com>
2025-11-27 08:51:59 +0530

    Update script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Theme Toggle
-const toggleBtn = document.getElementById("themeToggle");
-
-toggleBtn.onclick = () => {
-    document.body.classList.toggle("dark");
-    toggleBtn.innerText = document.body.classList.contains("dark")
-        ? "Dark"
-        : "Light";
-};
