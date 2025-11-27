970f00fcdb328754220c70cd61302a5e4756368b
Ashwani Kumar <5yberbind@gmail.com>
2025-11-27 14:13:17 +0530

    Delete source directory

--- source/src/pages/AppMarket.jsx
+++
@@ -1,8 +0,0 @@
-import apps from '../../public/data/apps.json';
-import { Link } from 'react-router-dom';
-export default ()=>(
-  <div>
-    <h1>Apps</h1>
-    {apps.map(a=><div key={a.id}><Link to={'/apps/'+a.id}>{a.name}</Link></div>)}
-  </div>
-);
--- source/src/pages/Home.jsx
+++
@@ -1,6 +0,0 @@
-export default ()=> (
-  <div>
-    <h1>Welcome to 5yberBind Marketplace</h1>
-    <p>Download apps, editing packs, templates & more.</p>
-  </div>
-);
