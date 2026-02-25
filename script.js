5291a76a6a321314550c36052add880f6cc5def9
Ashwani Kumar <5yberbind@gmail.com>
2026-02-26 04:16:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function filterCategory(cat) {
-  let cards = document.getElementsByClassName("card");
-  for (let i = 0; i < cards.length; i++) {
-    let c = cards[i].getAttribute("data-category");
-    cards[i].style.display = (cat === "all" || c === cat) ? "block" : "none";
-  }
+function scrollToPricing() {
+    document.getElementById("pricing").scrollIntoView({
+        behavior: "smooth"
+    });
 }
-<script>
-document.addEventListener("mousemove", (e) => {
-  const trail = document.createElement("div");
-  trail.className = "cursor-trail";
-  trail.style.left = e.clientX + "px";
-  trail.style.top = e.clientY + "px";
-  document.body.appendChild(trail);
-
-  setTimeout(() => {
-    trail.remove();
-  }, 300);
-});
-</script>
