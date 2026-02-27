5064fbec36c722778dbb74b4eb5c50f9fff1de0d
Ashwani Kumar <5yberbind@gmail.com>
2026-02-27 08:26:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function scrollToSection(id) {
         behavior: "smooth"
     });
 }
+// Theme Toggle
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+    
+    if(document.body.classList.contains("light-mode")){
+        toggleBtn.innerHTML = "☀️";
+    } else {
+        toggleBtn.innerHTML = "🌙";
+    }
+});
